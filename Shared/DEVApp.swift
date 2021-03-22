f73a094465891496bed1dd52cf30b2fad73df385
Hadi Dbouk <hadi.dbouk@snow.dog>
2021-03-22 21:21:53 +0200

    initial commit

---
+++ Shared/DEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEVApp.swift
+//  Shared
+//
+//  Created by Hadi on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
