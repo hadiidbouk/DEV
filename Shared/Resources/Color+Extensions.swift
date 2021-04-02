787b83d5001572a0322c208501d8136b7ef8f4a2
Hadi Dbouk <hadiidbouk@gmail.com>
2021-04-02 17:59:50 +0300

    #15 - Use appPrimary instead of the pre-defined primary color

--- Shared/Resources/Color+Extensions.swift
+++ Shared/Resources/Color+Extensions.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 extension Color {
     static let background = Color("background")
-    static let primary = Color("primary")
+    static let appPrimary = Color("primary")
     static let accent = Color("accent")
     static let primaryText = Color("primaryText")
     static let secondaryText = Color("secondaryText")
