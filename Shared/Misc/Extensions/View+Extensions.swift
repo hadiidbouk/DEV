033e0cb5f793d630c1ac8f8fd73ef288f1ba2219
Hadi Dbouk <hadiidbouk@gmail.com>
2021-04-02 18:02:48 +0300

    #15 - enhance 'on' view extension function

--- Shared/Misc/Extensions/View+Extensions.swift
+++ Shared/Misc/Extensions/View+Extensions.swift
@@ -8,22 +8,12 @@
 import SwiftUI
 
 extension View {
-    @ViewBuilder func on<Content: View>(_ platform: Platform, transform: (Self) -> Content) -> some View {
-        switch platform {
-        case .iOS:
-            #if os(iOS)
-                transform(self)
-            #else
-                self
-            #endif
-        case .macOS:
-            #if os(macOS)
-                transform(self)
-            #else
-                self
-            #endif
-        }
+    func on<Content: View>(_ platforms: Platform..., transform: (Self) -> Content) -> AnyView {
+        guard platforms.contains(Platform.current) else { return anyView }
+        return transform(self).anyView
     }
+
+    var anyView: AnyView { AnyView(self) }
 }
 
 extension View {
