a3da15121b68961e25df891970778fbafddd128e
Hadi Dbouk <hadiidbouk@gmail.com>
2021-04-08 00:07:12 +0300

    #23 - added DEVAPI package with Article class

---
+++ DEVAPI/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "DEVAPI",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "DEVAPI",
+            targets: ["DEVAPI"]),
+    ],
+    targets: [
+        .target(
+            name: "DEVAPI",
+            dependencies: []),
+        .testTarget(
+            name: "DEVAPITests",
+            dependencies: ["DEVAPI"]),
+    ]
+)
