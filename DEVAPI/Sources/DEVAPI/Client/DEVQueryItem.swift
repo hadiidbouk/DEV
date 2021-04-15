df19127ee14e0bf250a362a9c3e0615fc7c58269
Hadi Dbouk <hadiidbouk@gmail.com>
2021-04-16 00:28:26 +0300

    #26 - Added more DEVQueryItem

--- DEVAPI/Sources/DEVAPI/Client/DEVQueryItem.swift
+++ DEVAPI/Sources/DEVAPI/Client/DEVQueryItem.swift
@@ -10,11 +10,61 @@ import Foundation
 public enum DEVQueryItem: Equatable {
     case page(Int)
     case perPage(Int)
+    case tag(String)
+    case tags(String)
+    case tagsExclude(String)
+    case username(String)
+    case state(State)
+    case top(Int)
+    case collectionId(Int)
 
-    var urlQueryItem: URLQueryItem {
+    var urlQueryItem: URLQueryItem { .init(name: name, value: value) }
+
+    public enum State: String {
+        case fresh
+        case rising
+        case all
+    }
+}
+
+private extension DEVQueryItem {
+    var name: String {
         switch self {
-        case let .page(value):       return URLQueryItem(name: "page", value: String(value))
-        case let .perPage(value):    return URLQueryItem(name: "perPage", value: String(value))
+        case .page:
+            return "page"
+        case .perPage:
+            return "per_page"
+        case .tag:
+            return "tag"
+        case .tags:
+            return "tags"
+        case .tagsExclude:
+            return "tags_exclude"
+        case .username:
+            return "username"
+        case .state:
+            return "state"
+        case .top:
+            return "top"
+        case .collectionId:
+            return "collection_id"
+        }
+    }
+
+    var value: String {
+        switch self {
+        case .page(let value),
+             .perPage(let value),
+             .top(let value),
+             .collectionId(let value):
+            return String(value)
+        case .tag(let value),
+             .tags(let value),
+             .tagsExclude(let value),
+             .username(let value):
+            return value
+        case let .state(state):
+            return state.rawValue
         }
     }
 }
