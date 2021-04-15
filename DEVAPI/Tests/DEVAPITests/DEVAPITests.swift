d8b31807e61f9c70994adb6f0232075b1bf8c277
Hadi Dbouk <hadiidbouk@gmail.com>
2021-04-16 00:41:56 +0300

    #26 - Added test decoding for all articles API

--- DEVAPI/Tests/DEVAPITests/DEVAPITests.swift
+++ DEVAPI/Tests/DEVAPITests/DEVAPITests.swift
@@ -2,8 +2,19 @@ import XCTest
 @testable import DEVAPI
 
 final class ArticlesTests: XCTestCase {
-    func testDecoding() {
+    func testLatestDecoding() {
         let fileName = "articles-latest"
+        testArticlesDecoding(from: fileName)
+    }
+
+    func testAllDecoding() {
+        let fileName = "articles"
+        testArticlesDecoding(from: fileName)
+    }
+}
+
+private extension ArticlesTests {
+    func testArticlesDecoding(from fileName: String) {
         guard  let url = Bundle.module.url(forResource: fileName, withExtension: "json") else {
             XCTFail("Cannot find \(fileName).")
             return
@@ -12,7 +23,7 @@ final class ArticlesTests: XCTestCase {
         do {
             let data = try Data(contentsOf: url)
             let articles = try jsonDecoder.decode([Article].self, from: data)
-            XCTAssertEqual(articles.count, 30)
+            XCTAssertNotEqual(articles.count, 0)
         } catch let error {
             XCTFail(error.localizedDescription)
         }
