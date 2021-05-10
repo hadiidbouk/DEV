959d8ebc9e15c2d401e1b3336f4e0701071abefc
Hadi Dbouk <hadiidbouk@gmail.com>
2021-05-11 01:19:03 +0300

    #45 - added isHidden View extension function

--- Shared/Misc/Extensions/View+Extensions.swift
+++ Shared/Misc/Extensions/View+Extensions.swift
@@ -45,4 +45,14 @@ extension View {
         )
         .clipShape(shape)
     }
+
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
 }
